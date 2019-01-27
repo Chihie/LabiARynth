9f6c1c43f3643fc0e76c684fbabb7443be1588e6
Chihie <k.szamrowicz@gmail.com>
2019-01-27 20:12:28 +0100

    Update BehaviourDanger.cs

--- Assets/Scripts/BehaviourDanger.cs
+++ Assets/Scripts/BehaviourDanger.cs
@@ -7,35 +7,25 @@ public class BehaviourDanger : MonoBehaviour
 {
 
 	[SerializeField] private float invokeDelay = 0.5f;
-	private Material mat;//[] mats;
+	private Material mat;
 	private bool isRed = false;
 	
 	// Use this for initialization
 	void Start () {
 		mat = gameObject.GetComponent<MeshRenderer>().material;
-//		mats = renderer.material;
-		InvokeRepeating("Blink", 0, invokeDelay);
-//		Material[] mats = gameObject.GetComponent.<Renderer>().materials;
-//		mats[3] = mat2;
-//		Part4.GetComponent.<Renderer>().materials = mats;
-		
+		InvokeRepeating("Blink", 0, invokeDelay);		
 	}
 	
 	void Blink()
 	{
 		if (!isRed)
 		{
-			mat.color = Color.red;
-//			gameObject.SetActive(false);
-//			mats[0] = mats[1];
-			
+			mat.color = Color.red;			
 			isRed = true;
 		}
 		else
 		{
 			mat.color = Color.white;
-//			gameObject.SetActive(true);
-//			mats[0] = mats[2];
 			isRed = false;
 		}
 	}
